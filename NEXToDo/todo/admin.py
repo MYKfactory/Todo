b13ce2e55abae8518fe71b0565672af38823da9d
miya0603 <miya19900603@gmail.com>
2017-03-08 02:39:29 +0900

    delete old ver

--- NEXToDo/todo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import ResistedTodo
-from .models import Person
-from .models import Category
-
-admin.site.register(Person)
-admin.site.register(Category)
-admin.site.register(ResistedTodo)
